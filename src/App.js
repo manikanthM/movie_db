dd199abd9774afc5f4ebee7eb601de8343ee2289
manikanth <manikanta@Apples-MacBook-Pro.local>
2021-12-04 13:14:58 +0530

    project setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+/*  global React ReactDOM  */
+import React from "react";
 
 // components
-import Header from './components/Header'
-
+import Header from "./components/Header";
 
 //styles
-import {GlobalStyle} from './GlobalStyles'
+import { GlobalStyle } from "./GlobalStyles";
 // const start = () => React.createElement('div', null, "This used React createelement")
 function App() {
   return (
     <div className="App">
-      
       <Header />
       Start here.
       <GlobalStyle />
