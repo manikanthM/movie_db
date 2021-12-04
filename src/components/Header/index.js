e7eea326fb70664895fa1fd30ebe125f613f5535
Manikanta <manikanta@Apples-MacBook-Pro.local>
2021-12-04 11:52:39 +0530

    testing

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import TMDBLogo from '../../images/tmdb_logo.svg'
-import RMDBLogo from '../../images/react-movie-logo.svg'
+import TMDBLogo from "../../images/tmdb_logo.svg";
+import RMDBLogo from "../../images/react-movie-logo.svg";
 
-import {Wrapper, Content, LogoImg, TMDBLogoImg} from './Header.styles'
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
 
 const Header = () => (
-    <Wrapper >
-        <Content>
-            <LogoImg src = {RMDBLogo} alt = 'rect_img' />
-            <TMDBLogoImg src = {TMDBLogo} alt = 'db_logo' />
-        </Content>
-    </Wrapper>
+  <Wrapper>
+    <Content>
+      <LogoImg src={RMDBLogo} alt="rect_img" />
+      <TMDBLogoImg src={TMDBLogo} alt="db_logo" />
+    </Content>
+  </Wrapper>
 );
 
 export default Header;
